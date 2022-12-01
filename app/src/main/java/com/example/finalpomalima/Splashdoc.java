1b29f2661bd9de2f34177f1ac20e4aac88e2b667
Pomalima <71742554@continental.edu.pe>
2022-12-01 18:11:40 -0500

    final

--- app/src/main/java/com/example/finalpomalima/Splashdoc.java
+++ app/src/main/java/com/example/finalpomalima/Splashdoc.java
@@ -2,13 +2,34 @@ package com.example.finalpomalima;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Splashdoc extends AppCompatActivity {
 
+    TextView docente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashdoc);
+        docente = findViewById(R.id.txtdocente);
+        //PASAR LUEGO DE 3 SEGUNDOS
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                llamar_main(docente);
+            }
+        }, 3000);
+    }
+
+
+    public void llamar_main(View view){
+        Intent i = new Intent(Splashdoc.this, Logindoc.class);
+        startActivity(i);
     }
 }
