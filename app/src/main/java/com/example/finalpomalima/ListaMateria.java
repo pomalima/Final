81337ca705c0a095860ccbadc9e59bd85818ca0a
Pomalima <71742554@continental.edu.pe>
2022-12-04 23:48:21 -0500

    final

--- app/src/main/java/com/example/finalpomalima/ListaMateria.java
+++ app/src/main/java/com/example/finalpomalima/ListaMateria.java
@@ -2,13 +2,27 @@ package com.example.finalpomalima;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ListaMateria extends AppCompatActivity {
+
+    Button btnVolverListMateria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_materia);
+
+        btnVolverListMateria = findViewById(R.id.btnVolverListMateria);
+        btnVolverListMateria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ListaMateria.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/finalpomalima/RegistroMateria.java
+++ app/src/main/java/com/example/finalpomalima/RegistroMateria.java
@@ -2,13 +2,27 @@ package com.example.finalpomalima;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistroMateria extends AppCompatActivity {
+
+    Button btnCancelarReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_materia);
+
+        btnCancelarReg = findViewById(R.id.btnCancelarMat);
+        btnCancelarReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RegistroMateria.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
